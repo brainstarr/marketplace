3ac6c77fbd1b7393b1c072cc789f7ed9bd78c680
brainstarr <brian.s@airbnb.com>
2013-04-13 11:21:25 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marketplace::Application.config.secret_token = '5a1fb6662d96014e9e431e6f16ad421d5dc8134d4adc5dc364228df50c5ff7064d920f024acfb17d325e6d55594622f439ff5ba89f7eb2a2d50c920e78a58164'
