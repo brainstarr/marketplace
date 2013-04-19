02ae44e48810a6b667d07b0afaee55a23e733190
brainstarr <brian.s@airbnb.com>
2013-04-18 18:27:35 -0700

    added routes, controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_filter :verify_authentication!
+
   protect_from_forgery
+
+  private
+
+  def verify_authentication!
+    unless session[:id].present?
+      redirect_to users_sign_in_path
+    end
+  end
 end
